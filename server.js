56bf7fe3d9a0c2c629b69f0995ed72e9aa60a31d
ggg71 <ggg7152@gmail.com>
2022-01-03 16:51:15 +0900

    connect mongoDB

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : true}));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
+MongoClient.connect(
+  "mongodb+srv://admin:admin1234@cluster0.ir1mp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  function (에러, client) {
+    //연결되면 할 일
+    if (에러) return console.log(에러);
 
-MongoClient.connect('mongodb+srv://admin:admin1234@cluster0.ir1mp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-function(에러,client){
-    //연결되면 할 일 
-    if(에러) {return console.log(에러)}
-    app.listen(8080, function(){
-        console.log('listening on 8080');
+    db = client.db("todoapp");
+
+    db.collection("post").insertOne({ 이름: "John", _id: 100 }, function (에러, 결과) {
+        console.log("저장완료");
+      }
+    );
+
+    app.listen(8080, function () {
+      console.log("listening on 8080");
     });
-})
-
+  }
+);
 
 // /pet에 방문하면 관련된 안내문 출력
-app.get('/pet',function(요청, 응답){
-    응답.send('펫 용품 쇼핑할 수 있는 페이지입니다.')
+app.get("/pet", function (요청, 응답) {
+  응답.send("펫 용품 쇼핑할 수 있는 페이지입니다.");
 });
 
-app.get('/beauty',function(요청, 응답){
-    응답.send('뷰티 용품 쇼핑할 수 있는 페이지입니다.')
+app.get("/beauty", function (요청, 응답) {
+  응답.send("뷰티 용품 쇼핑할 수 있는 페이지입니다.");
 });
 
-app.get('/',function(요청, 응답){
-    응답.sendFile(__dirname + '/index.html')
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
 });
 
-app.get('/write',function(요청, 응답){
-    응답.sendFile(__dirname + '/write.html')
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
 });
 
-app.post('/add', function(요청,응답){
-    응답.send('전송완료')
-    console.log(요청.body.title)
-    console.log(요청.body.date)
-    //db에 저장해주세요
-
-})
+app.post("/add", function (요청, 응답) {
+  응답.send("전송완료");
+  console.log(요청.body.title);
+  console.log(요청.body.date);
+  //db에 저장해주세요
+});

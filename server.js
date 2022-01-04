d5841e4adf92729f62035754f3955efa4918a240
ggg71 <ggg7152@gmail.com>
2022-01-04 18:12:04 +0900

    refactor server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const MongoClient = require("mongodb").MongoClient;
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 MongoClient.connect(
   "mongodb+srv://admin:admin1234@cluster0.ir1mp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -40,39 +40,55 @@ app.get("/write", function (요청, 응답) {
 
 // 게시글 등록
 app.post("/add", function (요청, 응답) {
+  // 참고 : 응답.send()이 부분은 없으면 브라우저 멈춤
+  // 실패하든 성공하든 무조건 무언가 보내줘야 함.
   응답.send("전송완료");
   console.log(요청.body.title);
   console.log(요청.body.date);
 
-  //1. db에 counter컬렉션에 현재 총 게시물 갯수 데이터 찾아오기 
-  db.collection('counter').findOne({name : '게시물 갯수'},function(에러,결과){
-    console.log(결과.totalPost);
-    var 총게시물갯수 = 결과.totalPost;
+  //1. db에 counter컬렉션에 현재 총 게시물 갯수 데이터 찾아오기
+  db.collection("counter").findOne(
+    { name: "게시물 갯수" },
+    function (에러, 결과) {
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
 
-    //2. db에 todo id,제목,날짜 저장
-    db.collection('post').insertOne({_id : 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function(에러,결과) {
-        console.log('DB에 저장완료');
+      //2. db에 todo id,제목,날짜 저장
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        function (에러, 결과) {
+          console.log("DB에 저장완료");
 
-        //3. db에 counter 컬렉션에 총 게시물 갯수 1씩 증가
-        db.collection('counter').updateOne({name: '게시물 갯수'},{$inc : {totalPost:1}}, function(에러, 결과){
-          if(에러){return console.log(에러)}
-
-        })
-    });
-  });
-
+          //3. db에 counter 컬렉션에 총 게시물 갯수 1씩 증가
+          db.collection("counter").updateOne(
+            { name: "게시물 갯수" },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+            }
+          );
+        }
+      );
+    }
+  );
 });
 
-
-
 // 게시글 리스트
-app.get('/list', function(요청,응답){
-    
-    db.collection('post').find().toArray(function(에러, 결과){
-        console.log(결과);
-        응답.render('list.ejs', { posts : 결과 });
-    });
-    
-    //db에 저장된 post라는 이름을 가진 collection 안의 모든 데이터를 꺼내기.
+//db에 저장된 post라는 이름을 가진 collection 안의 모든 데이터를 보여주기
+// ejs파일로 보내줘야한다. 그냥 html파일만 보내주면 static페이지임.
+app.get("/list", function (요청, 응답) {
 
+  // 1. post 콜렉션에 저장된 모든 데이터를 Array자료형으로 가져온다.
+  db.collection("post")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+
+      // 2. 결과라는 데이터를 posts 라는 이름으로 list.ejs 파일에 보낸다.
+      응답.render("list.ejs", { posts: 결과 });
+    });
+
+  
 });

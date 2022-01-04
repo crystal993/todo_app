cb51041772c2251a7f3118e5c1f54f91317787f5
ggg71 <ggg7152@gmail.com>
2022-01-04 23:39:45 +0900

    change index.html to index.ejs

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/beauty", function (요청, 응답) {
 });
 
 app.get("/", function (요청, 응답) {
-  응답.sendFile(__dirname + "/index.html");
+  응답.render("index.ejs");
 });
 
 app.get("/write", function (요청, 응답) {

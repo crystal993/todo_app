ea7c34034c2afa323974a459959a13bbe5de18d1
ggg71 <ggg7152@gmail.com>
2022-01-10 15:15:31 +0900

    이미지 업로드 기능 완성

--- server.js
+++ server.js
@@ -37,12 +37,36 @@ app.get("/", function (요청, 응답) {
   응답.render("index.ejs");
 });
 
-
 // app.use() : 미들웨어 
 //미들웨어 : 요청과 응답사이에 실행되는 코드
 app.use('/shop', require('./routes/shop.js'));
 app.use('/', require('./routes/board.js'));
 
+//업로드 페이지로 이동
 app.get('/upload', function(request,response){
   response.render('upload.ejs');
 })
+
+// multer를 이용한 이미지 하드에 저장하기
+// npm install multer
+let multer = require('multer');
+var storage = multer.diskStorage({
+  // 이미지 업로드한 곳의 경로
+  destination : function(req, file, cb){
+    cb(null, './public/image');
+  },
+  // 저장한 이미지의 파일명 설정하는 부분
+  filename : function(req, file, cb){
+    cb(null, file.originalname);
+  }
+});
+
+var upload = multer({storage: storage});
+
+app.post('/upload', upload.single('profile'), function(request, response){
+  response.send('업로드완료');
+});
+
+app.get('/image/:imageName', function(request, response){
+  response.sendFile( __dirname + '/public/image' + request.params.imageName);
+});
